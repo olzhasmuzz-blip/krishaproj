6efec14f8f815387261d4122eb3e40696b45c742
olzhasmuzz-blip <olzhasmuzz@gmail.com>
2026-09-17 09:47:55 +0500

    Use BeautifulSoup for all Krisha HTML parsing

--- crawler/test_krisha_parser.py
+++ crawler/test_krisha_parser.py
@@ -7,6 +7,20 @@ SEARCH_HTML = '''
 <html><body><div class="a-card"><a href="/a/show/123456789"><h3>2-комнатная квартира</h3><div class="a-card__price">42 000 000 ₸</div><div class="a-card__main-info">2-комн. · 58 м² · 5/9 этаж</div><div class="a-card__address">Алматы, Бостандыкский р-н</div></a></div></body></html>
 '''
 
+KRISHA_CARD_HTML = '''
+<html><body><div class="a-card a-storage-live" data-id="987654321">
+  <a class="a-card__image" href="/a/show/987654321" title="Продажа квартиры">
+    <picture data-full-src="https://photos.example/full.jpg"><source srcset="https://photos.example/card.webp 1x"><img src="https://photos.example/card.jpg"></picture>
+  </a>
+  <div class="a-card__header"><a class="a-card__title" href="/a/show/987654321">3-комнатная квартира · 82 м² · 6/12 этаж</a>
+    <div class="a-card__price">67 500 000 ₸</div></div>
+  <div class="a-card__subtitle">Бостандыкский р-н, пр. Абая</div>
+  <div class="a-card__text-preview">Монолитный дом, свежий ремонт, рядом парк.</div>
+  <div class="a-card__owner-label">Хозяин недвижимости</div>
+  <div class="a-card__stats-item">Алматы</div>
+</div></body></html>
+'''
+
 
 class KrishaParserTest(unittest.TestCase):
     def test_search_page_extracts_listing_fields(self):
@@ -18,6 +32,21 @@ class KrishaParserTest(unittest.TestCase):
         self.assertEqual(items[0].area_m2, 58.0)
         self.assertEqual(items[0].floor, 5)
         self.assertEqual(items[0].floors_total, 9)
+
+    def test_live_krisha_card_extracts_structured_fields_not_whole_card_as_title(self):
+        parser = KrishaParser(min_interval_s=0)
+        items = parser.parse_search_page(KRISHA_CARD_HTML, page_url="https://krisha.kz/prodazha/kvartiry/almaty/")
+        self.assertEqual(len(items), 1)
+        item = items[0]
+        self.assertEqual(item.title, "3-комнатная квартира · 82 м² · 6/12 этаж")
+        self.assertEqual(item.price_kzt, 67_500_000)
+        self.assertEqual(item.rooms, 3)
+        self.assertEqual(item.area_m2, 82.0)
+        self.assertEqual((item.floor, item.floors_total), (6, 12))
+        self.assertEqual(item.city, "Алматы")
+        self.assertEqual(item.district, "Бостандыкский р-н")
+        self.assertIn("свежий ремонт", item.description)
+        self.assertIn("https://photos.example/full.jpg", item.photo_urls)
 
 
 if __name__ == "__main__":
